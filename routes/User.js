cdbe16fcc5af61eb30279d142df0b78e063d316c
Sushant-Gadyal <sushantgadyal19@gmail.com>
2024-04-21 17:13:45 +0530

    User profile Image updtd

--- routes/User.js
+++ routes/User.js
@@ -4,6 +4,8 @@ const {verifyToken}=require('../middleware/auth.js');
 const User =require('../models/User.js')
 const jwt=require('jsonwebtoken')
 const path = require('path');
+const upload = require("./multer.js");
+const { decode } = require('punycode');
 
 router.get("/profile",verifyToken,async(req, res) => {
   const token = req.cookies.authorization;
@@ -14,4 +16,63 @@ router.get("/profile",verifyToken,async(req, res) => {
     }
     res.render('user_service', {user});
   });
+
+
+
+router.post("/fileupload",verifyToken,upload.single("image"), async function(req,res){
+  const token = req.cookies.authorization;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const userobj = await User.findById(decoded.id).select('-password');
+  if(!userobj){
+    return res.status(404).json({error:"User not found"});
+  }
+  userobj.profile_pic = req.file.filename;
+  await userobj.save();
+  res.redirect("/users/profile");
+});
+
+
+router.get("/user_park",verifyToken,async function(req,res){
+
+  const token = req.cookies.authorization;
+  const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.id).select("-password");
+
+  if(!user){
+    return res.status(404).json({error : "User not found"});
+  }
+
+  res.render("user_parking",{user});
+});
+
+
+router.get("/user_current_bookings",verifyToken,async function(req,res){
+
+  const token = req.cookies.authorization;
+  const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.id).select("-password");
+
+  if(!user){
+    return res.status(404).json({error : "User not found"});
+  }
+
+  res.render("user_current-bookings",{user});
+});
+
+router.get("/user_wallet",verifyToken,async function(req,res){
+
+  const token = req.cookies.authorization;
+  const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.id).select("-password");
+
+  if(!user){
+    return res.status(404).json({error : "User not found"});
+  }
+
+  res.render("user_wallet",{user});
+});
+
 module.exports = router;
