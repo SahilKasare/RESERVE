19c89923fa96b1a225f0bd0bfee62eb5276437d9
Sushant-Gadyal <sushantgadyal19@gmail.com>
2024-04-25 18:38:57 +0530

    Manager Multer done

--- routes/Manager.js
+++ routes/Manager.js
@@ -6,7 +6,7 @@ const jwt=require('jsonwebtoken')
 const path = require('path');
 const upload = require("./multer.js");
 const { decode } = require('punycode');
-const Manager=require('../models/Manager')
+const Manager=require('../models/Manager');
 
 router.get("/dashboard",verifyToken,async(req, res) => {
     const token = req.cookies.authorization;
@@ -16,7 +16,19 @@ router.get("/dashboard",verifyToken,async(req, res) => {
         return res.status(404).json({ error: "User not found" });
       }
       res.render('manager_dashboard', {manager});
-    });
+});
   
+router.post("/fileupload",verifyToken,upload.single("image"), async function(req,res){
+  const token = req.cookies.authorization;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const manager = await Manager.findById(decoded.id).select('-password');
+
+  if(!manager){
+    return res.status(404).json({error:"User not found"});
+  }
+  manager.profile_pic = req.file.filename;
+  await manager.save();
+  res.redirect("/managers/dashboard");
+});
 
 module.exports = router;
